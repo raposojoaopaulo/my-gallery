ffc18ef1f4a3867f336fece8d150427800d31662
Joao Paulo Raposo <raposojoaopaulo@gmail.com>
2022-09-06 21:12:59 -0300

    created upload and discard for photos

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,12 +1,13 @@
 import { Photo } from '../types/Photo'
 import { storage } from '../libs/firebase'
-import { ref, listAll, getDownloadURL } from 'firebase/storage'
+import { ref, listAll, getDownloadURL, uploadBytes, deleteObject } from 'firebase/storage'
+import { v4 } from 'uuid'
 
 export const getAll = async () => {
   let list: Photo[] = [];
 
   const imagesFolder = ref(storage, "images");
-  const photoList = await listAll(imagesFolder)
+  const photoList = await listAll(imagesFolder);
 
   for(let i in photoList.items) {
     let photoUrl = await getDownloadURL(photoList.items[i])
@@ -18,4 +19,26 @@ export const getAll = async () => {
   }
 
   return list;
+}
+
+export const insert = async (file: File) => {
+  if(['image/jpeg', 'image/jpg', 'image/png'].includes(file.type)) {
+    let randonName = v4();
+    let newFile = ref(storage, `images/${randonName}`);
+
+    let upload = await uploadBytes(newFile, file);
+    let photoUrl = await getDownloadURL(upload.ref);
+
+    return {
+      name: upload.ref.name,
+      url: photoUrl
+    } as Photo;
+  } else {
+    return new Error('Invalid file!');
+  }
+}
+
+export const discard = async (name: string) => {
+  let discardedPhoto = ref(storage, `images/${name}`);
+  await deleteObject(discardedPhoto);
 }
